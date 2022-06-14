b839b21734ad0f5d75ae1db7404948a6f9d4f29e
Varun chandrashekar <varun@Varuns-MacBook-Air.local>
2022-06-14 11:29:21 +0530

    basic game working without checkmate

--- testgui.c
+++ testgui.c
@@ -3,265 +3,242 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
 
-int check_pos(int row, int column);
-int is_capital(char letter);
-int get_indices(char* pos);
-void display();
-int input(int moving);
-void initialize();
 void rook(int r, int c);
 void bishop(int r, int c);
+void pawn(int r, int c);
+void horse(int r, int c);
+int check_pos(int row, int column);
+int is_capital(char letter);
+int input(int moving);
+void display(int display_moves);
+void initialize();
+void get_moves(char piece, int r, int c);
 
 char current_player[] = "White";
 char board[8][8] = {};
 int moves[8][8] = {};
+int check = 0;
 
+// Valid moves for the rook
 void rook(int r, int c) {
-	int count = 1;
+	int checked = 0;
+	bool u_flag = true, d_flag = true, l_flag = true, r_flag = true;
 
-	while(1) {
-		if(r-count >= 0) {
-			if(board[r-count][c] != '\0') {
-				if(!strcmp(current_player, "White") && !is_capital(board[r-count][c]) || !strcmp(current_player, "Black") && is_capital(board[r-count][c])) {
-					moves[r-count][c] = 2;
-				}
-			} else {
-				moves[r-count][c] = 1;
-				count++;
-				continue;
-			}
-		}
-		break;
-	}
+	for(int i = 1; i<8; i++) {
+		checked = check_pos(r-i, c);
 
-	count=1;
-	while(1) {
-		if(r+count < 8) {
-			if(board[r+count][c] != '\0') {
-				if(!strcmp(current_player, "White") && !is_capital(board[r+count][c]) || !strcmp(current_player, "Black") && is_capital(board[r+count][c])) {
-					moves[r+count][c] = 2;
-				}
-			} else {
-				moves[r+count][c] = 1;
-				count++;
-				continue;
+		if((r-i) >= 0 && u_flag){
+			if(!checked || (checked == 2)) {
+				moves[r-i][c] = checked;
+				u_flag = false;
 			}
+			moves[r-i][c] = checked;
 		}
-		break;
-	}
 
-	count=1;
-	while(1) {
-		if(c-count >= 0) {
-			if(board[r][c-count] != '\0') {
-				if(!strcmp(current_player, "White") && !is_capital(board[r][c-count]) || !strcmp(current_player, "Black") && is_capital(board[r][c-count])) {
-					moves[r][c-count] = 2;
-				}
-			} else {
-				moves[r][c-count] = 1;
-				count++;
-				continue;
+		checked = check_pos(r+i, c);
+		if((r+i) < 8 && d_flag){
+			if(!checked || (checked == 2)) {
+				moves[r+i][c] = checked;
+				d_flag = false;
 			}
+			moves[r+i][c] = checked;
 		}
-		break;
-	}
+
+		checked = check_pos(r, c-i);
+		if((c-i) >= 0 && l_flag){
+			if(!checked || (checked == 2)) {
+				moves[r][c-i] = checked;
+				l_flag = false;
+			}
+			moves[r][c-i] = checked;
+		}
 		
-	count=1;
-	while(1) {
-		if(c+count < 8) {
-			if(board[r][c+count] != '\0') {
-				if(!strcmp(current_player, "White") && !is_capital(board[r][c+count]) || !strcmp(current_player, "Black") && is_capital(board[r][c+count])) {
-					moves[r][c+count] = 2;
-				}
-			} else {
-				moves[r][c+count] = 1;
-				count++;
-				continue;
+		checked = check_pos(r, c+i);
+		if((c+i) < 8 && r_flag){
+			if(!checked || (checked == 2)) {
+				moves[r][c+i] = checked;
+				r_flag = false;
 			}
+			moves[r][c+i] = checked;
 		}
-		break;
 	}
 }
 
+// Valid moves for the bishop
 void bishop(int r, int c) {
-	int count = 1;
+	int checked = 0;
+	bool ul_flag = true, ur_flag = true, ll_flag = true, lr_flag = true;
 
-	while(1) {
-		if((r-count >= 0) && (c-count >= 0)) {
-				
-			if(board[r-count][c-count] != '\0') {
-				if(!strcmp(current_player, "White") && !is_capital(board[r-count][c-count]) || !strcmp(current_player, "Black") && is_capital(board[r-count][c-count])) {
-					moves[r-count][c-count] = 2;
-				}
-			} else {
-				moves[r-count][c-count] = 1;
-				count++;
-				continue;
-			}
-		}
-		break;
-	}
-	
-	count=1;
-	while(1) {
-		if((r-count >= 0) && (c+count < 8)) {
-			if(board[r-count][c+count] != '\0') {
-				if(!strcmp(current_player, "White") && !is_capital(board[r-count][c+count]) || !strcmp(current_player, "Black") && is_capital(board[r-count][c+count])) {
-					moves[r-count][c+count] = 2;
-				}
-				break;
-			} else {
-				moves[r-count][c+count] = 1;
-				count++;
-				continue;
-			}
-		}
-		break;
-	}
+	for(int i = 1; i<8; i++) {
+		checked = check_pos(r-i, c-i);
 
-	count=1;
-	while(1) {
-		if((r+count < 8) && (c-count >= 0)) {
-			if(board[r+count][c-count] != '\0') {
-				if(!strcmp(current_player, "White") && !is_capital(board[r+count][c-count]) || !strcmp(current_player, "Black") && is_capital(board[r+count][c-count])) {
-					moves[r+count][c-count] = 2;
-				}
-				break;
-			} else {
-				moves[r+count][c-count] = 1;
-				count++;
-				continue;
+		if((r-i) >= 0 && (c-i) >= 0 && ul_flag){
+			if(!checked || (checked == 2)) {
+				moves[r-i][c-i] = checked;
+				ul_flag = false;
 			}
+			moves[r-i][c-i] = checked;
 		}
-		break;
-	}
+
+		checked = check_pos(r-i, c+i);
+		if((r-i) >= 0 && (c+i) <8 && ur_flag){
+			if(!checked || (checked == 2)) {
+				moves[r-i][c+i] = checked;
+				ur_flag = false;
+			}
+			moves[r-i][c+i] = checked;
+		}
+
+		checked = check_pos(r+i, c-i);
+		if((r+i) < 8 && (c-i) >= 0 && ll_flag){
+			if(!checked || (checked == 2)) {
+				moves[r+i][c-i] = checked;
+				ll_flag = false;
+			}
+			moves[r+i][c-i] = checked;
+		}
 		
-	count=1;
-	while(1) {
-		if((r+count < 8) && (c+count < 8)) {
-			if(board[r+count][c+count] != '\0') {
-				if(!strcmp(current_player, "White") && !is_capital(board[r+count][c+count]) || !strcmp(current_player, "Black") && is_capital(board[r+count][c+count])) {
-					moves[r+count][c+count] = 2;
-				}
-				break;
-			} else {
-				moves[r+count][c+count] = 1;
-				count++;
-				continue;
+		checked = check_pos(r+i, c+i);
+		if((r+i) < 8 && (c+i) < 8 && lr_flag){
+			if(!checked || (checked == 2)) {
+				moves[r+i][c+i] = checked;
+				lr_flag = false;
 			}
+			moves[r+i][c+i] = checked;
 		}
-		break;
 	}
 }
 
+// Valid moves for horse
 void horse(int r, int c) {
-	if((r-2 >= 0) && (c+1 < 8) && (board[r-2][c+1] != '\0')) {
-		if(!strcmp(current_player, "White") && !is_capital(board[r-2][c+1]) || !strcmp(current_player, "Black") && is_capital(board[r-2][c+1])) {
-			moves[r-2][c+1] = 2;
-		}
-	} else {
-			moves[r-2][c+1] = 1;
-	}
 
-	if((r-2 >= 0) && (c-1 >= 0) && (board[r-2][c-1] != '\0')) {
-		if(!strcmp(current_player, "White") && !is_capital(board[r-2][c-1]) || !strcmp(current_player, "Black") && is_capital(board[r-2][c-1])) {
-			moves[r-2][c-1] = 2;
-		}
-	} else {
-			moves[r-2][c-1] = 1;
-	}
+	int c_sign = -1, r_sign = -1, checked = 0;
 
-	if((r+2 < 8) && (c-1 >= 0) && (board[r+2][c-1] != '\0')) {
-		if(!strcmp(current_player, "White") && !is_capital(board[r+2][c-1]) || !strcmp(current_player, "Black") && is_capital(board[r+2][c-1])) {
-			moves[r+2][c-1] = 2;
-		}
-	} else {
-			moves[r+2][c-1] = 1;
-	}
+	for(int i = 0; i<2; i++) {
+		for(int j = 0; j<2; j++) {
 
-	if((r+2 < 8) && (c+1 < 8) && (board[r+2][c+1] != '\0')) {
-		if(!strcmp(current_player, "White") && !is_capital(board[r+2][c+1]) || !strcmp(current_player, "Black") && is_capital(board[r+2][c+1])) {
-			moves[r+2][c+1] = 2;
+			checked = check_pos(r+(r_sign*2), c+(c_sign*1));
+			printf("(%d, %d); checked: %d\n", r+(r_sign*2), c+(c_sign*1), checked);
+			if(checked == 1 || checked == 2)
+				moves[r+(r_sign*2)][c+(c_sign*1)] = checked;
+			
+			checked = check_pos(r+(c_sign*1), c+(r_sign*2));
+			printf("(%d, %d); checked: %d\n", r+(c_sign*1), c+(r_sign*2), checked);
+			if(checked == 1 || checked == 2)
+				moves[r+(c_sign*1)][c+(r_sign*2)] = checked;
+			
+			c_sign *= -1;
 		}
-	} else {
-			moves[r+2][c+1] = 1;
+		r_sign *= -1;
 	}
 }
 
+// Valid moves for the queen
 void queen(int r, int c) {
 	bishop(r, c);
 	rook(r, c);
 }
 
+// Valid moves for the king
 void king(int r, int c) {
-	int j = -1;
-	for(int i = r-1; i<=(r+1); i++) {
-		if(i<0 || i>7 || (i==r && j==0))
-			continue;
-		if(board[i][c+j] != '\0') {
-			if(!strcmp(current_player, "White") && !is_capital(board[i][c+j]) || !strcmp(current_player, "Black") && is_capital(board[i][c+j])) {
-				moves[i][c+j] = 2;
-			} else {
-				moves[i][c+j] = 1;
-			}
-		}
 
+	enum directions {U, D, L, R, UL, UR, DL, DR};
+
+	typedef struct {
+		int row;
+		int col;
+	} cell;
+
+	cell possible_moves[8];
+	int checked = 0;
+
+	possible_moves[U] = (cell){r+1, c};
+	possible_moves[D] = (cell){r-1,  c};
+	possible_moves[L] = (cell){r,    c-1};
+	possible_moves[R] = (cell){r,    c+1};
+	possible_moves[UL] = (cell){r+1, c-1};
+	possible_moves[UR] = (cell){r+1, c+1};
+	possible_moves[DL] = (cell){r-1, c-1};
+	possible_moves[DR] = (cell){r-1,  c+1};
+
+	for(int i = 0; i<8;i++) {
+		checked = check_pos(possible_moves[i].row, possible_moves[i].col);
+		if(checked == 2 || checked == 1)
+			moves[possible_moves[i].row][possible_moves[i].col] = checked;
 	}
+	moves[r][c] = 3;
 }
 
+// Valid moves for pawn
 void pawn(int r, int c) {
-	if(!strcmp(current_player, "White")) {
-		if(board[r-1][c] == '\0') {
-			moves[r-1][c] = 1;
-		}
-		if(r==6)
-			if(board[r-2][c] == '\0') {
-				moves[r-2][c] = 1;
-		}
-		if((board[r-1][c-1] != '\0') && !is_capital(board[r-1][c-1]))
-			moves[r-1][c-1] = 2;
-		if((board[r-1][c+1] != '\0') && !is_capital(board[r-1][c+1]))
-			moves[r-1][c+1] = 2;
-	} else {
-		if(board[r+1][c] == '\0') {
-			moves[r+1][c] = 1;
-		}
-		if(r==1)
-			if(board[r+2][c] == '\0') {
-				moves[r+2][c] = 1;
-		}
-		if((board[r+1][c-1] != '\0') && is_capital(board[r+1][c-1]))
-			moves[r+1][c-1] = 2;
-		if((board[r+1][c+1] != '\0') && is_capital(board[r+1][c+1]))
-			moves[r+1][c+1] = 2;
-	}
+
+	int sign = 1;
+
+	if(!strcmp(current_player, "Black"))
+		sign *= -1;
+		
+	if(board[r-(1*sign)][c] == '\0')
+		moves[r-(1*sign)][c] = 1;
+	if(((r==6) && !strcmp(current_player, "White")) || ((r==1) && !strcmp(current_player, "Black")))
+		if(board[r-(2*sign)][c] == '\0')
+			moves[r-(2*sign)][c] = 1;
+
+	if(check_pos(r-(1*sign), c-(1*sign*-1)) == 2)
+		moves[r-(1*sign)][c-(1*sign*-1)] = 2;
+	if(check_pos(r-(1*sign), c+(1*sign*-1)) == 2)
+		moves[r-(1*sign)][c+(1*sign*-1)] = 2;
 }
+
+// void check() {
+// 	int pos[2] = {};
+// 	for(int i = 0; i<8; i++) {
+// 		for(int j = 0; j<8; j++) {
+// 			if(!strcmp(current_player,"White") && (board[i][j] == 'K')) {
+// 				pos[0] = i;
+// 				pos[1] = j;
+// 			} else if(!strcmp(current_player,"Black") && (board[i][j] == 'k')) {
+// 				pos[0] = i;
+// 				pos[1] = j;
+// 			}
+// 		}
+// 	}
+
+// 	rook(pos[0], pos[1]);
+// 	for(int i = 0; i<8; i++) {
+// 		for(int j = 0; j<8; j++) {
+// 			if(moves[i][j] == 2)
+				
+// 		}
+// 	}
+// }
 
 //Perform checks to see if position is valid
 int check_pos(int row, int column) {
-	//printf("\n\n%c\n\n", board[row][column]);
 
-	if(board[row][column] == '\0') {
-		printf("\nThat position is empty\n");
-		return 0;
-	} else if((column < 0) || (column > 7) || (row < 0) || (row > 7)) {
-		printf("\nThat position is not on the board!\n");
-		return 0;
-	} else if((current_player == "White") && !is_capital(board[row][column]) || (current_player == "Black") && is_capital(board[row][column])) {
-		printf("\nYou cannot move that piece\n");
-		return 0;
+	if((column < 0) || (column > 7) || (row < 0) || (row > 7)) {
+		// Out of board
+		return -1;
+	} else if (board[row][column] == '\0') {
+		// Empty
+		return 1;
+	} else if(!strcmp(current_player, "White") && !is_capital(board[row][column]) || !strcmp(current_player, "Black") && is_capital(board[row][column])) {
+		// Opponent piece
+		return 2;
 	}
 
-	return 1;
+	// Player piece
+	return 0;
 }
 
 //Check if letter is capital
 int is_capital(char letter) {
 	if(((int)letter >= (int)'A') && ((int)letter <= (int)'Z'))
-		return 1;
+		return true;
 	else
-		return 0;
+		return false;
 }
 
 //Get input from the user and check if its valid
@@ -274,7 +251,7 @@ int input(int moving) {
 		//Display prompts
 		printf("\nEnter the column and row of a pawn to move ( CR ): ");
 		scanf("%2s", s);
-		if(!check_pos(s[1]-49, s[0]-65) && !moving)
+		if(check_pos(s[1]-49, s[0]-65) && !moving)
 			continue;
         break;
 	}
@@ -326,7 +303,7 @@ void display(int display_moves) {
 
 //Set the board to its starting position
 void initialize() {
-	char *pawns = "rhbkqbhr";
+	char *pawns = "rhbqkbhr";
 
 	for(int i = 0; i< 8; i++) {
 		for(int j = 0; j<8; j++) {
@@ -393,7 +370,7 @@ void get_moves(char piece, int r, int c) {
 //char message[] = "White's turn";
 int main() {
 
-    while(1) {
+    while(true) {
 
         system("@cls||clear");
         printf("----------------\n");
@@ -420,7 +397,7 @@ int main() {
     start:
     initialize();
 
-    while(1){
+    while(true){
 
         system("@cls||clear");
         printf("%s's turn\n", current_player);
